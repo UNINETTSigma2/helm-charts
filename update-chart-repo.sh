49bd6c21e3021fa4480742a63705feb8acf5bde4
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-11-06 11:02:11 +0100

    Attempt to make the linting less noisy.

--- update-chart-repo.sh
+++ update-chart-repo.sh
@@ -4,8 +4,7 @@ charts=( "dokuwiki" "spark/spark" "wordpress" "etherpad" "jupyter" )
 
 for chart in "${charts[@]}"
 do
-    echo "Linting $chart..."
-    helm lint --strict $chart
+    helm lint --strict $chart | grep -v "linted"
     echo
 done
 
@@ -13,6 +12,5 @@ for chart in "${charts[@]}"
 do
     echo "Packaging $chart..."
     helm package $chart --destination docs/
-    echo
 done
 helm repo index docs --url https://UNINETT.github.com/helm-charts
