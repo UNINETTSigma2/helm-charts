9a44f4e90fadaf755086672d262d65c62a5744a9
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-11-06 09:14:13 +0100

    Lint all charts before packaging them.

--- update-chart-repo.sh
+++ update-chart-repo.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+charts=( "dokuwiki" "spark/spark" "wordpress" "etherpad" "jupyter" )
 
-for chart in "dokuwiki" "spark/spark" "wordpress" "etherpad" "jupyter"
+for chart in "${charts[@]}"
 do
+    echo "Linting $chart..."
+    helm lint $chart
+    echo
+done
+
+for chart in "${charts[@]}"
+do
+    echo "Packaging $chart..."
     helm package $chart --destination docs/
+    echo
 done
 helm repo index docs --url https://UNINETT.github.com/helm-charts
