2628279ece5bfd65706218f616f0dc9b2bc62a43
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-01-25 13:15:38 +0100

    Make the user specify the schema and port when connecting to the spark master.

--- spark/dockerfiles/notebook/start-notebook.sh
+++ spark/dockerfiles/notebook/start-notebook.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 
 unset SPARK_MASTER_PORT
 
-if [ -z ${SPARK_MASTER_SERVICE_HOST+x} ]; then
+if [ -z ${SPARK_MASTER_SERVICE+x} ]; then
 	echo "spark.master local[1]" >> $SPARK_HOME/conf/spark-defaults.conf
 else
-	echo "spark.master spark://${SPARK_MASTER_SERVICE_HOST:-spark-master}:${SPARK_MASTER_SERVICE_PORT:-7077}" >> $SPARK_HOME/conf/spark-defaults.conf
+	echo "spark.master ${SPARK_MASTER_SERVICE}" >> $SPARK_HOME/conf/spark-defaults.conf
 fi
 echo "spark.driver.memory ${SPARK_DRIVER_MEMORY:-1g}" >> $SPARK_HOME/conf/spark-defaults.conf
 echo "spark.driver.cores ${SPARK_DRIVER_CORES:-1}" >> $SPARK_HOME/conf/spark-defaults.conf
