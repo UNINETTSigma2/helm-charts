7861b65045b230ea61e57b9ee33f903a570269d7
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-04-09 21:14:05 +0200

    Move all docker related files to https://github.com/UNINETT/helm-charts-dockerfiles

--- jupyterlab/image/start-notebook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Strict mode
-set -euo pipefail
-
-jupyter lab --config $NOTEBOOKS_CONFIG_DIR/jupyter_notebook_config.py --ip 0.0.0.0 --port 8888
--- update-chart-repo.sh
+++ update-chart-repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -o pipefail
-charts=( "dokuwiki" "spark/spark" "wordpress" "etherpad" "jupyter" "jupyterhub/jupyterhub" "jupyterlab/jupyterlab" "deep-learning-tools" )
+charts=( "dokuwiki" "spark" "wordpress" "etherpad" "jupyter" "jupyterhub" "jupyterlab" "deep-learning-tools" )
 
 for chart in "${charts[@]}"
 do
