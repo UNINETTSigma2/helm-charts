7de6a5a8d20fb462c893206b72a1075deb1d4ebf
Travis CI <travis@travis-ci.org>
2018-05-03 21:56:31 +0200

    Attempt to make Travis automatically update the repo docs.

---
+++ push.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Copied from: https://gist.github.com/willprice/e07efd73fb7f13f917ea
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git add . docs/
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  #git remote add origin https://${GH_TOKEN}@github.com/UNINETT/helm-charts.git > /dev/null 2>&1
+  #git push --quiet --set-upstream origin master
+  echo "pushing"
+}
+
+setup_git
+commit_website_files
+upload_files
