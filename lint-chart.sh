df6ca4d9191c02391466885462b7088f6bb87409
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-05-05 23:52:09 +0200

    Ensure that a proper status code is returned even though no match is found.

--- lint-chart.sh
+++ lint-chart.sh
@@ -6,5 +6,5 @@ echo "Linting $1..."
 echo "Running Helm lint..."
 helm lint --strict $1 | grep -vE "linted|Lint"
 echo "Running Kubeval lint..."
-helm template $1 | kubeval --strict | grep -E "invalid|(-->)"
-echo "No errors were found by the linters!"
+helm template $1 | kubeval --strict | (grep -v " valid" || true)
+echo "Yay, no errors when linting $1"
