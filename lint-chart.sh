adf4c4981d452acf4c4b928ded5561ce6f3f3016
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-05-05 23:41:38 +0200

    Attempt to make the linting script output less useless info.

--- lint-chart.sh
+++ lint-chart.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
-helm lint --strict $1 | grep -v "linted"
-helm template $1 | kubeval --strict
+#!/bin/bash
+set -e
+set -o pipefail
+
+echo "Linting $1..."
+echo "Running Helm lint..."
+helm lint --strict $1 | grep -vE "linted|Lint"
+echo "Running Kubeval lint..."
+helm template $1 | kubeval --strict | grep -E "invalid|(-->)"
+echo "No errors were found by the linters!"
