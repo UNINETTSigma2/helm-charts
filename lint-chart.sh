201dcaacdf8b8e8e4d2d85a732eab012d5fb20ca
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-05-06 10:54:03 +0200

    Start using kubetest for testing the helm charts.

--- lint-chart.sh
+++ lint-chart.sh
@@ -5,6 +5,9 @@ set -o pipefail
 echo "Linting $1..."
 echo "Running Helm lint..."
 helm lint --strict $1 | grep -vE "linted|Lint"
-echo "Running Kubeval lint..."
-helm template $1 | kubeval --strict | (grep -v " valid" || true)
+helm template $1 > deployment.yaml
+echo "Running kubeval lint..."
+kubeval --strict <deployment.yaml | (grep -v " valid" || true)
+echo "Running kubetest lint..."
+kubetest --verbose <deployment.yaml | (grep -v "info" || true)
 echo "Yay, no errors when linting $1"
