f7fb79e7aed85ac98692a590123bcd412e68a813
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-07-16 14:31:08 +0200

    Move stable charts to a stable repo, and testing charts to a testing repo, as this makes it easier to see which charts to use.

--- build/update-chart-repo.sh
+++ build/update-chart-repo.sh
@@ -2,21 +2,27 @@
 set -e
 set -o pipefail
 
-charts=()
-while IFS= read -r -d $'\0'; do
-    charts+=("$REPLY")
-done < <(find -- * -type d -exec sh -c '[ -f "$0"/Chart.yaml ]' '{}' \; -print0)
-
-for chart in "${charts[@]}"
+for repo in repos/*/
 do
-        ./lint-chart.sh $chart
-	echo
-done
+    repo=${repo%*/}
+    charts=()
+    while IFS= read -r -d $'\0'; do
+	charts+=("$REPLY")
+    done < <(find -- $repo/* -type d -exec sh -c '[ -f "$0"/Chart.yaml ]' '{}' \; -print0)
 
-helm init -c
-for chart in "${charts[@]}"
-do
-    echo "Packaging $chart..."
-    helm package $chart --destination docs/
+    for chart in "${charts[@]}"
+    do
+	    ./lint-chart.sh $chart
+	    echo
+    done
+
+    repo_dir="docs/$repo"
+    mkdir -p $repo_dir
+    helm init -c
+    for chart in "${charts[@]}"
+    do
+	echo "Packaging $chart..."
+	helm package $chart --destination $repo_dir
+    done
+    helm repo index $repo_dir --url https://Uninett.github.com/helm-charts/$repo
 done
-helm repo index docs --url https://Uninett.github.com/helm-charts
