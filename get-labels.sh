2d384c033f392fe28aaf443cc4d052bcdaba85e9
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-07-19 12:08:05 +0200

    Don't use /tmp as travis does not seem to like using it.

--- get-labels.sh
+++ get-labels.sh
@@ -21,10 +21,10 @@ do
     MANIFEST_ID="$(curl -sq -L "$API_URL/repository/$REPO/$IMAGE/tag/?specificTag=$IMG_TAG" | jq -r '.tags[0].manifest_digest')"
     LABELS="$(curl -sq -L "$API_URL/repository/$REPO/$IMAGE/manifest/$MANIFEST_ID/labels" | jq '.labels | [.[] | select( .key | startswith("pkg"))] | {"packageVersions": map({(.key): .value}) }')"
     if [[ -v $LABELS ]]; then
-	echo "$LABELS" > /tmp/labels-tmp.json
-	ALL_LABELS="$(jq -s '.[0] * .[1]' /tmp/labels.json /tmp/labels-tmp.json)"
-	echo "$ALL_LABELS" > /tmp/labels.json
+	echo "$LABELS" > labels-tmp.json
+	ALL_LABELS="$(jq -s '.[0] * .[1]' labels.json labels-tmp.json)"
+	echo "$ALL_LABELS" > labels.json
     fi
 done
-cat $CHART_YAML | yq '.' > /tmp/chart.json
-yq -s -y '.[0] * .[1]' /tmp/chart.json /tmp/labels.json > $CHART_YAML
+cat $CHART_YAML | yq '.' > chart.json
+yq -s -y '.[0] * .[1]' chart.json labels.json > $CHART_YAML
