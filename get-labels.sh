daba10e852d6ca0df934f247de9ad05e4e1fa030
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-07-19 12:16:08 +0200

    Make sure to initialize the labels.json file with a default value.

--- get-labels.sh
+++ get-labels.sh
@@ -12,6 +12,7 @@ if [[ -z $IMAGES ]]; then
     exit 0
 fi
 
+echo "{}" > labels.json
 for i in $IMAGES
 do
     IFS=':' read -a parts <<< "${i}"
@@ -20,11 +21,10 @@ do
 
     MANIFEST_ID="$(curl -sq -L "$API_URL/repository/$REPO/$IMAGE/tag/?specificTag=$IMG_TAG" | jq -r '.tags[0].manifest_digest')"
     LABELS="$(curl -sq -L "$API_URL/repository/$REPO/$IMAGE/manifest/$MANIFEST_ID/labels" | jq '.labels | [.[] | select( .key | startswith("pkg"))] | {"packageVersions": map({(.key): .value}) }')"
-    if [[ -v $LABELS ]]; then
-	echo "$LABELS" > labels-tmp.json
-	ALL_LABELS="$(jq -s '.[0] * .[1]' labels.json labels-tmp.json)"
-	echo "$ALL_LABELS" > labels.json
-    fi
+    echo "$LABELS" > labels-tmp.json
+    ALL_LABELS="$(jq -s '.[0] * .[1]' labels.json labels-tmp.json)"
+    echo "$ALL_LABELS" > labels.json
 done
+
 cat $CHART_YAML | yq '.' > chart.json
 yq -s -y '.[0] * .[1]' chart.json labels.json > $CHART_YAML
