4ba0967b191e9d9ed8d1862e2e8f532c58888493
Thierry Toutain <thierry.toutain@usit.uio.no>
2021-09-24 15:11:31 +0200

    update schema location

--- lint-chart.sh
+++ lint-chart.sh
@@ -2,15 +2,12 @@
 set -e
 set -o pipefail
 
-echo $PATH
-which helm
-which kubeval
 echo "Linting $1..."
 echo "Running Helm lint..."
 helm lint --strict $1 | grep -vE "linted|Lint"
 helm template $1 > deployment.yaml
 echo "Running kubeval lint..."
-kubeval -v 1.19.15 --strict <deployment.yaml | (grep -v " valid" || true)
+kubeval -v 1.19.15 -s https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master --strict <deployment.yaml | (grep -v " valid" || true)
 echo "Running kubetest lint..."
 cat deployment.yaml | conftest test -
 echo "Yay, no errors when linting $1"
