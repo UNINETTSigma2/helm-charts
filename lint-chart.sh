ee77ac304321774ca7956b14c78724aa593c05b9
Torkel Lyng <torkel.lyng@gmail.com>
2023-08-15 09:51:06 +0200

    Update lint-chart.sh
    Disabled kubeval in lint verification.

--- lint-chart.sh
+++ lint-chart.sh
@@ -6,8 +6,8 @@ echo "Linting $1..."
 echo "Running Helm lint..."
 helm lint --strict $1 | grep -vE "linted|Lint"
 helm template $1 > deployment.yaml
-echo "Running kubeval lint..."
-kubeval -v 1.19.15 -s https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master < deployment.yaml | (grep -v " valid" || true)
+# echo "Running kubeval lint..."
+# kubeval -v 1.19.15 -s https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master < deployment.yaml | (grep -v " valid" || true)
 echo "Running kubetest lint..."
 cat deployment.yaml | conftest test -
 echo "Yay, no errors when linting $1"
